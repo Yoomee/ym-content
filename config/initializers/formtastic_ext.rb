29e14ff296536db3de3248af5f56293024513fe3
Matt Atkins <matt@yoomee.com>
2014-02-07 11:09:19 +0000

    Add content_input form helper method

---
+++ config/initializers/formtastic_ext.rb
@@ -0,0 +1,17 @@
+module Formtastic
+  module Helpers
+    module InputHelper
+
+      def content_input(content_attribute, options = {})
+        options.reverse_merge!(
+          :as => content_attribute.field_type,
+          :label => content_attribute.name,
+          :hint => content_attribute.description,
+          :required => content_attribute.required
+        )
+        input(content_attribute.slug, options)
+      end
+
+    end
+  end
+end
