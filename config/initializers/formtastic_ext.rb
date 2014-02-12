4d42645b7a2bd4013688e19e4c96a5919c45e67c
Greg Woodcock <greg@yoomee.com>
2014-02-12 12:07:44 +0000

    Make text content attributes redator controls

--- config/initializers/formtastic_ext.rb
+++ config/initializers/formtastic_ext.rb
@@ -4,7 +4,7 @@ module Formtastic
 
       def content_input(content_attribute, options = {})
         options.reverse_merge!(
-          :as => content_attribute.field_type,
+          :as => content_attribute.field_type == 'text' ? 'redactor' : content_attribute.field_type,
           :label => content_attribute.name,
           :hint => content_attribute.description,
           :required => content_attribute.required
