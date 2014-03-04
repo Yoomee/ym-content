3f1e024660bf29bcda5feb25974d113ce894c936
Matt Atkins <matt@yoomee.com>
2014-03-04 10:57:46 +0000

    Fix link text inputs

--- app/inputs/link_input.rb
+++ app/inputs/link_input.rb
@@ -1,2 +1,9 @@
 class LinkInput < FormtasticBootstrap::Inputs::StringInput
+
+  def to_html
+    bootstrap_wrapping do
+      builder.text_field(method, form_control_input_html_options.reverse_merge(:value => builder.object.send(method).url).merge(:id => "content_package_#{method}_url"))
+    end
+  end
+
 end
