97b49a4192699fcd2b890f5d1e3988a6e3a2a2bf
Matt Atkins <matt@yoomee.com>
2014-03-04 12:33:20 +0000

    Don't error when link nil

--- app/inputs/link_input.rb
+++ app/inputs/link_input.rb
@@ -2,7 +2,7 @@ class LinkInput < FormtasticBootstrap::Inputs::StringInput
 
   def to_html
     bootstrap_wrapping do
-      builder.text_field(method, form_control_input_html_options.reverse_merge(:value => builder.object.send(method).url).merge(:id => "content_package_#{method}_url"))
+      builder.text_field(method, form_control_input_html_options.reverse_merge(:value => builder.object.send(method).try(:url)).merge(:id => "content_package_#{method}_url"))
     end
   end
 
