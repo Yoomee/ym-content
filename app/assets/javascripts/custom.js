1527d4143b1e289ac47a510272a8ac10b2f04a00
Carrie Jackson <carrie@yoomee.com>
2014-06-25 13:00:16 +0100

    Content types, content types page added styling for forms, expandable attribute boxes, fix personas

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-  $('.control-label').click(function(){
-    $(this).next('.form-wrapper').toggle();
-  });
-  $('.persona-summary').hide()
-  $('.more-persona').click(function(){
+  $('.more-persona').on('click', function(){
     $(this).parent('.row').next('.persona-summary').toggle();
+  });
+  $('#content_attributes').on('click', '.nested-fields .reveal', function(e){
+    e.preventDefault();
+    $(this).next('.hide-it').slideToggle();
+    if($('.hide-it').is(':visible')){
+      $(this).text('Show less');
+    }
+    else{
+      $(this).text('Show more');
+    }
   });
 });
