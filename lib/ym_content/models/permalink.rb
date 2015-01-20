22984ba53fbb19208ba6252f223add71055ff981
Greg Woodcock <greg@yoomee.com>
2015-01-20 11:53:23 +0000

    Put the Permalink model back to how it was
    The permalink model was all commented out and just replaced with the version
    from ym_permalinks. This changes it back so we can catch any problems early on
    v.

--- lib/ym_content/models/permalink.rb
+++ lib/ym_content/models/permalink.rb
@@ -1,90 +1,10 @@
 class Permalink < ActiveRecord::Base
 
-  # include YmCore::Model
-
-  # belongs_to :resource, :polymorphic => true
-
-  # validates :full_path, :presence => true, :uniqueness => {:case_sensitive => false, :scope => :active}
-
-  # validate :path_does_not_match_existing_route
-  # validate :path_is_valid_url
-
-  # after_update :create_inactive_permalink
-  # after_update :delete_duplicate_permalinks
-
-  # def initialize(*args)
-  #   super(*args)
-  #   generate_unique_path!
-  # end
-
-  # def generate_unique_path!(title = resource.to_s)
-  #   if path.blank? && title.present?
-  #     path_name_root = title.to_url.parameterize
-  #     unique_path_name = path_name_root.dup
-  #     permalinks = new_record? ? self.class : self.class.where("id != ?",self.id)
-  #     count = 0
-  #     while permalinks.exists?(:path => unique_path_name)
-  #       count += 1
-  #       unique_path_name = "#{path_name_root}-#{count}"
-  #     end
-  #     self.path = unique_path_name
-  #   end
-  # end
-
-  # def resource_path
-  #   "/#{resource_type.tableize}/#{resource_id}"
-  # end
-
-  # def self.find_from_url(url)
-  #   find_by_attr = YmContent.config.nested_permalinks ? :full_path : :path
-  #   permalink_path = "#{find_by_attr == :full_path ? '/' : ''}#{url}".squeeze('/').downcase
-  #   # find_by_<method> is deprecated in rails 4
-  #   if Rails::VERSION::MAJOR >= 4
-  #     Permalink.find_by(find_by_attr => permalink_path)
-  #   else
-  #     Permalink.send("find_by_#{find_by_attr}", permalink_path)
-  #   end
-  # end
-
-  # private
-  # def create_inactive_permalink
-  #   return unless path_changed?
-  #   resource.permalinks.create(:active => false, :path => path_was, :full_path => full_path_was)
-  # end
-
-  # def delete_duplicate_permalinks
-  #   Permalink.without(self).where(:path => path, :full_path => full_path, :active => false).delete_all
-  # end
-
-  # def path_does_not_match_existing_route
-  #   existing_routes = Rails.application.routes.routes.collect do |route|
-  #     if YmContent.config.nested_permalinks
-  #       route.path.spec.to_s.split(/\(/).try(:[],1)
-  #     else
-  #       route.path.spec.to_s.split(/\/|\(/).try(:[],1)
-  #     end
-  #   end.uniq.compact
-  #   if existing_routes.include?(path)
-  #     errors.add(:path, "has already been taken")
-  #   end
-  # end
-
-  # def path_is_valid_url
-  #   if path.present? && (path != path.to_url.parameterize)
-  #     errors.add(:path, "is invalid")
-  #   end
-  # end
-
-
-
-  #THIS FIX IS SPECIFIC TO V, DO NOT DEPLOY TO GG
-  #ANY QUESTIONS ASK GREG
-
   include YmCore::Model
 
   belongs_to :resource, :polymorphic => true
 
-  validates :path, :presence => true, :uniqueness => {:case_sensitive => false, :scope => :active}
+  validates :full_path, :presence => true, :uniqueness => {:case_sensitive => false, :scope => :active}
 
   validate :path_does_not_match_existing_route
   validate :path_is_valid_url
@@ -115,18 +35,34 @@ class Permalink < ActiveRecord::Base
     "/#{resource_type.tableize}/#{resource_id}"
   end
 
+  def self.find_from_url(url)
+    find_by_attr = YmContent.config.nested_permalinks ? :full_path : :path
+    permalink_path = "#{find_by_attr == :full_path ? '/' : ''}#{url}".squeeze('/').downcase
+    # find_by_<method> is deprecated in rails 4
+    if Rails::VERSION::MAJOR >= 4
+      Permalink.find_by(find_by_attr => permalink_path)
+    else
+      Permalink.send("find_by_#{find_by_attr}", permalink_path)
+    end
+  end
+
   private
   def create_inactive_permalink
-    resource.permalinks.create(:active => false, :path => path_was) if path_changed?
+    return unless path_changed?
+    resource.permalinks.create(:active => false, :path => path_was, :full_path => full_path_was)
   end
 
   def delete_duplicate_permalinks
-    Permalink.without(self).where(:path => path, :active => false).delete_all
+    Permalink.without(self).where(:path => path, :full_path => full_path, :active => false).delete_all
   end
 
   def path_does_not_match_existing_route
     existing_routes = Rails.application.routes.routes.collect do |route|
-      route.path.spec.to_s.split(/\/|\(/).try(:[],1)
+      if YmContent.config.nested_permalinks
+        route.path.spec.to_s.split(/\(/).try(:[],1)
+      else
+        route.path.spec.to_s.split(/\/|\(/).try(:[],1)
+      end
     end.uniq.compact
     if existing_routes.include?(path)
       errors.add(:path, "has already been taken")
@@ -138,6 +74,5 @@ class Permalink < ActiveRecord::Base
       errors.add(:path, "is invalid")
     end
   end
-
 
 end
