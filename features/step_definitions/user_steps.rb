7c9a804a5bbf1c241060c3900377197facac9ddf
Claire Gowler <claire@yoomee.com>
2015-04-08 15:39:01 +0100

    Fix test

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -67,6 +67,7 @@ When(/^I set the user as inactive$/) do
   btn = page.find(".user-row[data_user-id='#{@user.id}']").find('.user-actions').find('.set-active-btn')
   expect(btn).not_to be_nil
   btn.click
+  sleep(2)
 end
 
 Then(/^the user is inactive$/) do
